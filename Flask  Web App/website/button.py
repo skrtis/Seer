1c774d8c7fd29da3bb5cf6ab12b99b8d260caffc
kurtis <kurtisang@gmail.com>
2024-12-04 18:04:37 -0500

    updated

--- Flask  Web App/website/button.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, request
-
-homebut = Blueprint('homebut',__name__)
-
-@homebut.route('/')
-def homebutton():
-    return render_template("home.html")
-
-
